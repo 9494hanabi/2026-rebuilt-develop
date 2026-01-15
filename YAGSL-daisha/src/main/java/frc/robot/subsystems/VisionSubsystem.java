b3d3a2bb17e3e7d3ffcb2709ae13f1dc429d94c7
Pai314 <pai314.eequoalmc2@gmail.com>
2026-01-15 20:52:14 +0900

    fix repository

---
+++ YAGSL-daisha/src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import swervelib.SwerveDrive;
+import frc.robot.lib.LimelightHelpers;
+import frc.robot.util.Constants;
+
+public class VisionSubsystem extends SubsystemBase {
+  private final SwerveDrive swerveDrive;
+  private final String limelightlName = Constants.VisionConstans.limeligtName;
+
+  public VisionSubsystem(SwerveDrive swerve) {
+    this.swerveDrive = swerve;
+  }
+
+  @Override
+  public void periodic() {
+    // ① YAGSLの「信頼できる見出し角」をLimelightへ送る（毎周期）
+    double yawDeg = this.swerveDrive.getOdometryHeading().getDegrees();
+    LimelightHelpers.SetRobotOrientation(limelightlName, yawDeg, 0, 0, 0, 0, 0);
+
+    // ② MegaTag2の推定値（wpiblue）を取得
+    var mt2 = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(limelightlName);
+
+    // ③ タグが見えてる時だけYAGSLへ融合
+    if (mt2.tagCount > 0) {
+      swerveDrive.addVisionMeasurement(mt2.pose, mt2.timestampSeconds);
+    }
+  }
+}
