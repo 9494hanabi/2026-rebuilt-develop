e548b882345cef5c1d8302aec89fadb73553ef9d
Suzuki Haruta <haruta@suzukinoMacBook-Air.local>
2026-01-22 19:58:22 +0900

    feat(drive): auto - AutoCommand実装の途中 [buggy]
    feat(drive): auto - AutoCommand実装の途中 [buggy]
    
    Status: バグ有り

--- YAGSL-setsuna/src/main/java/frc/robot/Robot.java
+++ YAGSL-setsuna/src/main/java/frc/robot/Robot.java
@@ -1,6 +1,8 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -12,9 +14,23 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
   private final RobotContainer m_robotContainer;
 
+  //以下スマートダッシュボードを使ったAutoのこと
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+
   public Robot() {
     m_robotContainer = new RobotContainer();
     System.out.println("RobotContainer" +  m_robotContainer);
+
+    //smartdashboadで出てくる選択肢
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+
+    // スマートダッシュボードにウェジットを追加する（名前：Auto setting）
+    SmartDashboard.putData("Auto setting", m_chooser);
   }
 
   @Override
@@ -28,8 +44,30 @@ public class Robot extends TimedRobot {
   @Override
   public void disabledPeriodic() {}
 
+  //　Init：一度だけ呼ばれるプログラム
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousInit() {
+    //始まる時にダッシュボードで選択する → m_autoSelectedに代入
+    m_autoSelected = m_chooser.getSelected();
+    System.out.println("Auto selected: " + m_autoSelected);
+  }
+
+  // Periodic：エネイブル中に約20msごとに繰り返し呼ばれる関数
+  @Override
+  public void autonomousPeriodic() {
+    //選択内容によっての分岐
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        // カスタム自律の動き
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        // カスタム自律の動き
+        break;
+    }
+  }
 
   @Override
   public void teleopInit() {
