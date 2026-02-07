69f2f0bf3ad20e7d7d46d4d849c6f76b6b52a7b1
Suzuki Haruta <haruta@suzukinoMacBook-Air.local>
2026-02-07 18:42:38 +0900

    refactor(misc): 例: クライム - 例: L1に登る機能を作成した。 [stable]
    refactor(misc): 例: クライム - 例: L1に登る機能を作成した。 [stable]
    
    Status: 安定

--- YAGSL-setsuna/src/main/java/frc/robot/Robot.java
+++ YAGSL-setsuna/src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.lib.util.LogCleanupService;
 
 
 // === 担当者 ===
